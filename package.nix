75588bf4f14a1d4dcc47186d86a935cff0ae0dde
Daniel Barlow <dan@telent.net>
2019-01-04 09:47:57 +0000

    add ffi_reflect
    * we're not using it for anything right now, but we probably will
    do and it saves me having to work out the sha256 again later

--- package.nix
+++ package.nix
@@ -17,6 +17,13 @@ let
     url = "https://raw.githubusercontent.com/kikito/inspect.lua/master/inspect.lua";
     sha256 = "1xk42w7vwnc6k5iiqbzlnnapas4fk879mkj36nws2p2w03nj5508";
   };
+  ffi_reflect = fetchFromGitHub {
+    owner = "luapower";
+    repo = "ffi_reflect";
+    name = "ffi_reflect";
+    rev  = "9eeeb18a474656c917cc28812d667637cc2896b8";
+    sha256 = "17wyvb7qwvr8j07zrj8a4kspfisqds6zfif3v4p2448p4xywscqs";
+  };
   fennel = fetchFromGitHub {
     owner = "bakpakin";
     name = "fennel";
@@ -29,7 +36,7 @@ in stdenv.mkDerivation {
   version = "0.0.1";
   src = ./.;
   FENNEL = "${fennel}/fennel";
-  LUA_PATH = "${fennel}/?.fnl.lua;${fennel}/?.lua;;";
+  LUA_PATH = "${fennel}/?.fnl.lua;${fennel}/?.lua;${ffi_reflect}/?.lua;;";
   LUA_CPATH = "${wayland}/lib/lib?.so;${libxkbcommon}/lib/lib?.so;${wlroots}/lib/lib?.so;;";
 
   WLROOTS = "${wlroots}";
